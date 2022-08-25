a4fa60d0bc0953104113b27753fcbbf58c1b4a7d
baljeet singh <prince.singh3998@gmail.com>
2022-08-25 17:58:13 +0530

    uservalidation

---
+++ Phase 3/userAuthentication/src/main/java/com/simplilearn/userAuthentication/AuthenticationUser.java
@@ -0,0 +1,22 @@
+package com.simplilearn.userAuthentication;
+
+public class AuthenticationUser {
+	
+	
+	public String username()
+	{
+		String email = "thealex@gmail.com";
+		return email;
+	}
+	
+	public String paswd()
+	{
+		String password = "helloiamalex";
+		return password;
+	}
+	
+	
+	
+	
+
+}
