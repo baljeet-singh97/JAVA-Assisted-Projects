4515ad16cbce60f1b70ca9f37c9c3f54465d270f
baljeet singh <prince.singh3998@gmail.com>
2022-09-29 22:07:26 +0530

    automateNG

---
+++ Phase 5/AutomateWebApplicationUsingTestNG/TestNgExample/src/test/java/simplilearn/com/testng/FacebookLogin.java
@@ -0,0 +1,37 @@
+package simplilearn.com.testng;
+
+import org.openqa.selenium.By; 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FacebookLogin {
+	
+	WebDriver driver;
+	@Test
+	public void facebook() {
+		
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("nikunj@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("Nikunj@123");
+		driver.findElement(By.name("login")).submit();
+		
+		
+	}
+	@BeforeMethod
+	public void beforeMethod() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\princ\\eclipse-workspace\\Phase 5\\chromedriver_win32\\chromedriver.exe");
+		driver= new ChromeDriver();
+	}
+	@AfterMethod
+	public void afterMethod() {
+		//driver.close();
+		driver=null;
+	}
+	
+	
+}
